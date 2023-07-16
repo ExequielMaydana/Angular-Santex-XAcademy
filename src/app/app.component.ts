ae4eecf68feda3869a26a343bc794508f8348bfa
Exe Developer <exedevcoding22@gmail.com>
2023-07-16 08:22:00 -0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface NamesAndCosts {
+  name: string;
+  cost: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'exercism_angular';
+
+  // Declaración del arreglo de items
+  items: NamesAndCosts[] = [
+    { name: 'Exequiel', cost: '1' },
+    { name: 'Santex', cost: '2' },
+    { name: 'XAcademy', cost: '3' },
+  ];
+
+  viewList: boolean = true;
+
+  listHidden() {
+    this.viewList ? (this.viewList = false) : (this.viewList = true);
+  }
 }
